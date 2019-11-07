e98ca19b81063e1318f1293ce83d5136767fcecc
Liljoe5525 <56942177+Liljoe5525@users.noreply.github.com>
2019-11-06 20:18:18 -0500

    Add files via upload
    Lab 3 still working on

---
+++ IT1050- Lab3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SpeedLimit = 35;
+            int Speed = 42;
+            if (Speed > SpeedLimit)
+            {
+                Console.WriteLine(" SLOW NOW ");
+            }
+
+    
+    }
+  }
+}
