67b00e365925c39a47a8652a380b719187f13832
Liljoe5525 <56942177+Liljoe5525@users.noreply.github.com>
2019-12-13 22:39:11 -0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7.css
+{
+    class Book
+    {
+        private string Title;
+        private string Author;
+        private int Year;
+        public Book(string Title, string Author, int year)
+        {
+            this.Title = Title;
+            this.Author = Author;
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0} writtenby {1}\n", Title, Author);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab7.css
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book("Hatchet", "Gary Paulen", 1987);
+
+            myBook.Display();
+        }
+    }
+}
